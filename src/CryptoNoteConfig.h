94af8175a0d92025306f464be1fbf93cc1d620a7
kidmantra <sparta0916@gmail.com>
2017-03-17 22:05:23 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -73,8 +73,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 //TODO Put here the name of your currency
-const char     CRYPTONOTE_NAME[]                             = "Diablo";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "Diablo";
+const char     CRYPTONOTE_NAME[]                             = "Equinox";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "Equinox";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
